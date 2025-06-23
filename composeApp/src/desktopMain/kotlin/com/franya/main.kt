5e786604bff293ecf444cd4b5ed6837dc05bcf24
yarik <ydpolo@mail.ru>
2025-06-23 13:32:57 +0300

    init

---
+++ composeApp/src/desktopMain/kotlin/com/franya/main.kt
@@ -0,0 +1,13 @@
+package com.franya
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "nyamuse",
+    ) {
+        App()
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/franya/Constants.kt
@@ -0,0 +1,3 @@
+package com.franya
+
+const val SERVER_PORT = 8080
